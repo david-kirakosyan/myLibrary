46cab1bd32b595be5b88a85b5753cc9a29e7a908
David <davidkirakosyan504@gmail.com>
2023-05-02 13:39:27 +0400

    myLibrary

--- src/main/java/com/example/mylibrary/constants/SharedConstants.java
+++ src/main/java/com/example/mylibrary/constants/SharedConstants.java
@@ -2,6 +2,6 @@ package com.example.mylibrary.constants;
 
 public interface SharedConstants {
 
-    String UPLOAD_FOLDER_AUTHOR = "D:/java2023/my-library/images/author/";
-    String UPLOAD_FOLDER_BOOK = "D:/java2023/my-library/images/book/";
+    String UPLOAD_FOLDER_AUTHOR = "D:/images/author/";
+    String UPLOAD_FOLDER_BOOK = "D:/images/book/";
 }
